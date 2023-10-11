d1b9a65e9e0e21dc4f4ab98c340c8137b11c51e1
cono200 <tatattian04@gmail.com>
2023-10-11 12:25:51 -0700

    Agregar archivos de proyecto.

---
+++ Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicios11Octubre
+{
+    internal class Class1
+    {
+        public void matrizbOOL()
+        {
+            // bool[,] verdadfalso = new bool[3,2];
+
+            bool[,] verdadfalso = { { true, false }, { false, false }, { true, true } };
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine(verdadfalso[i, j]);
+                }
+            }
+        }
+
+
+        public void SumarArray()
+        {
+            int[,] numero1 = { { 1, 2 }, { 3, 4 }, { 4, 5 } };
+            int[,] numero2 = { { 2, 3 }, { 5, 6 }, { 9, 11 } };
+
+            int[,] resultado = new int[3, 2];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+
+                    resultado[i, j] = numero1[i, j] + numero2[i, j];
+
+
+                    Console.WriteLine(resultado[i, j]);
+
+                }
+
+            }
+        }
+
+        public void MultiplicarArray()
+        {
+            int[,] numero1 = { { 1, 2 }, { 3, 4 }, { 4, 5 } };
+            int[,] numero2 = { { 2, 3 }, { 5, 6 }, { 9, 11 } };
+
+            int[,] resultado = new int[3, 2];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+
+                    resultado[i, j] = numero1[i, j] * numero2[i, j];
+
+
+                    Console.WriteLine(resultado[i, j]);
+
+                }
+
+            }
+        }
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ejercicios11Octubre
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class12 = new Class1();
+
+
+            //class12.matrizbOOL();
+
+            //   class12.SumarArray();
+
+            class12.MultiplicarArray();
+        }
+    }
+}
